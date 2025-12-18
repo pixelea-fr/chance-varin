e62209322b0b53d346aab35d64ceef5f29baec11
pixelea-fr <equipe@pixelea.fr>
2025-12-18 15:39:56 +0100

    Header chance varin

---
+++ patterns/header-chance-varin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Header Chance Varin
+ * Slug: ng1-base/header-chance-varin
+ * Categories: header
+ * Keywords: Up, chance , varin, pixelea, header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Navigation | Logo Centered Container"},"align":"full","className":"navigation-logo-centered__container","style":{"spacing":{"padding":{"right":"var:preset|spacing|5","left":"var:preset|spacing|5"}}},"layout":{"type":"constrained","contentSize":"1400px"}} -->
+<div class="wp-block-group alignfull navigation-logo-centered__container" style="padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5)"><!-- wp:navigation {"ref":111,"overlayBackgroundColor":"base","overlayTextColor":"contrast","align":"wide","className":"navigation-logo-centered","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} /--></div>
+<!-- /wp:group -->
