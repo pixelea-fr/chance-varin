b28a63a2fc1ef78f7f9e54f31cb8529c80a13a4f
pixelea-fr <equipe@pixelea.fr>
2026-01-15 16:22:59 +0100

    ajustement menu mobile non open

--- patterns/header-chance-varin.php
+++ patterns/header-chance-varin.php
@@ -8,5 +8,7 @@
  */
 ?>
 <!-- wp:group {"metadata":{"name":"Navigation | Logo Centered Container"},"align":"full","className":"navigation-logo-centered__container","style":{"spacing":{"padding":{"right":"var:preset|spacing|5","left":"var:preset|spacing|5"}}},"layout":{"type":"constrained","contentSize":"1400px"}} -->
-<div class="wp-block-group alignfull navigation-logo-centered__container" style="padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5)"><!-- wp:navigation {"ref":111,"align":"wide","className":"navigation-logo-centered is-style-main","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} /--></div>
+<div class="wp-block-group alignfull navigation-logo-centered__container" style="padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5)"><!-- wp:site-logo {"className":"only-mobitab"} /-->
+
+<!-- wp:navigation {"ref":111,"align":"wide","className":"navigation-logo-centered is-style-main","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} /--></div>
 <!-- /wp:group -->
