b9632b08e37c1f1b01c145d6e03ea888525f509b
pixelea-fr <equipe@pixelea.fr>
2026-01-15 11:06:36 +0100

    ajout de padding mobile

--- patterns/section-information.php
+++ patterns/section-information.php
@@ -7,8 +7,8 @@
  * Block Types: core/group
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Section Information"},"align":"full","className":"section-information has-padding-l-120 has-padding-r-120","layout":{"type":"constrained","contentSize":"1680px"}} -->
-<div class="wp-block-group alignfull section-information has-padding-l-120 has-padding-r-120"><!-- wp:media-text {"align":"","mediaId":420,"mediaLink":"<?php echo home_url(); ?>/?attachment_id=420","mediaType":"image","imageFill":true,"className":"is-style-style4 section-information__media-txt"} -->
+<!-- wp:group {"metadata":{"name":"Section Information"},"align":"full","className":"section-information has-padding-l-120 has-padding-r-120 has-padding-mobile-l-0 has-padding-mobile-r-0","layout":{"type":"constrained","contentSize":"1680px"}} -->
+<div class="wp-block-group alignfull section-information has-padding-l-120 has-padding-r-120 has-padding-mobile-l-0 has-padding-mobile-r-0"><!-- wp:media-text {"align":"","mediaId":420,"mediaLink":"<?php echo home_url(); ?>/?attachment_id=420","mediaType":"image","imageFill":true,"className":"is-style-style4 section-information__media-txt"} -->
 <div class="wp-block-media-text is-stacked-on-mobile is-image-fill-element is-style-style4 section-information__media-txt"><figure class="wp-block-media-text__media"><img src="<?php echo home_url(); ?>/wp-content/uploads/2026/01/bureau-1024x683.jpg" alt="" class="wp-image-420 size-full" style="object-position:50% 50%"/></figure><div class="wp-block-media-text__content"><!-- wp:group {"className":"is-style-style4 section-information__media-txt__txt","style":{"spacing":{"blockGap":"var:preset|spacing|5","padding":{"top":"var:preset|spacing|5","bottom":"var:preset|spacing|5"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group is-style-style4 section-information__media-txt__txt" style="padding-top:var(--wp--preset--spacing--5);padding-bottom:var(--wp--preset--spacing--5)"><!-- wp:heading {"style":{"typography":{"lineHeight":"1.25"}}} -->
 <h2 class="wp-block-heading" style="line-height:1.25">Besoin d’informations ?</h2>
