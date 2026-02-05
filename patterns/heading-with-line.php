608d624a4d8f91a196d257d786879bfc12c291c7
pixelea-fr <equipe@pixelea.fr>
2026-02-05 15:14:05 +0100

    ajustement heading line

--- patterns/heading-with-line.php
+++ patterns/heading-with-line.php
@@ -6,10 +6,10 @@
  * Block Types: core/heading
  */
 ?>
-<!-- wp:group {"metadata":{"name":"is-style-with-line | Container"},"align":"full","className":"is-style-with-line__container is-style-style1","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull is-style-with-line__container is-style-style1"><!-- wp:group {"metadata":{"name":"is-style-with-line | Wrapper"},"align":"full","className":"is-style-with-line__wrapper","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull is-style-with-line__wrapper"><!-- wp:heading {"placeholder":"Lorem ipsum dolor…","align":"full","className":"is-style-with-line","fontSize":"xxl"} -->
-<h2 class="wp-block-heading alignfull is-style-with-line has-xxl-font-size">Pont l'évèque</h2>
+<!-- wp:group {"metadata":{"name":"is-style-with-line | Container"},"align":"full","className":"is-style-with-line__container is-style-style1 has-padding-l-120 has-padding-r-120","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull is-style-with-line__container is-style-style1 has-padding-l-120 has-padding-r-120"><!-- wp:group {"metadata":{"name":"is-style-with-line | Wrapper"},"align":"full","className":"is-style-with-line__wrapper","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull is-style-with-line__wrapper"><!-- wp:heading {"placeholder":"Lorem ipsum dolor…","align":"full","className":"is-style-with-line has-padding-l-32 has-padding-r-32","fontSize":"xxl"} -->
+<h2 class="wp-block-heading alignfull is-style-with-line has-padding-l-32 has-padding-r-32 has-xxl-font-size">Pont l'évèque</h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
