d12571d972bffe37cb3164ab2b61d5bbe7c823d6
pixelea-fr <equipe@pixelea.fr>
2026-02-11 16:19:41 +0100

    ajout d'un media text decal 6 duotone

---
+++ patterns/media-text-type-6-duotone.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Media Texte Type 6
+ * Slug: ng1-base/media-text-type-6-duotone
+ * Categories: media-text, up
+ * Keywords: pixelea, up ,ng1, media, texte ,image, portrait
+ * Block Types: core/group
+ */
+?>
+<!-- wp:group {"align":"full","className":"has-padding-l-120 has-padding-r-120 media-text-6__container","layout":{"type":"constrained","contentSize":"1920px"}} -->
+<div class="wp-block-group alignfull has-padding-l-120 has-padding-r-120 media-text-6__container"><!-- wp:columns {"verticalAlignment":"bottom","className":"media-text-6__columns","style":{"spacing":{"blockGap":{"left":"0"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-bottom media-text-6__columns"><!-- wp:column {"verticalAlignment":"bottom","width":"","className":"media-text-6__column media-text-6__column\u002d\u002d1","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-bottom media-text-6__column media-text-6__column--1"><!-- wp:group {"className":"is-style-style1 has-z-index-2 has-padding-r-240 has-padding-l-120 has-padding-t-120 has-padding-b-120 has-padding-mobile-t-40 has-padding-mobile-b-40","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"border":{"width":"1px"}},"borderColor":"base-4","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-style1 has-z-index-2 has-padding-r-240 has-padding-l-120 has-padding-t-120 has-padding-b-120 has-padding-mobile-t-40 has-padding-mobile-b-40 has-border-color has-base-4-border-color" style="border-width:1px;margin-top:0;margin-bottom:0"><!-- wp:heading {"className":"is-style-default","fontSize":"xl"} -->
+<h2 class="wp-block-heading is-style-default has-xl-font-size">Quelques exemples de missions…</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Notaire associé au sein de l’étude Chancé-Varin &amp; Associés à Pont-l’Évêque depuis 2017, Maître Thomas HOULEY intervient dans tous les domaines du notariat, avec une expertise particulière en immobilier et en promotion immobilière. Il accompagne ses clients dans la réalisation d’opérations complexes, en apportant des solutions adaptées aux enjeux juridiques et financiers de chaque projet.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Partenaire de confiance de plusieurs family offices, il conseille ses clients sur les stratégies d’organisation patrimoniale et les accompagne dans leurs acquisitions, tant au niveau local que national. Son expertise s’étend notamment à l’achat et à la gestion de grands domaines et de haras, apportant un savoir-faire précieux à une clientèle exigeante soucieuse d’optimiser et de sécuriser son patrimoine.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","className":"media-text-6__column media-text-6__column\u002d\u002d2"} -->
+<div class="wp-block-column is-vertically-aligned-bottom media-text-6__column media-text-6__column--2"><!-- wp:image {"id":985,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo home_url(); ?>/wp-content/uploads/2026/02/713dfe3c582e3eb2d6e1b3af39206a15201b181a.png" alt="" class="wp-image-985"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
