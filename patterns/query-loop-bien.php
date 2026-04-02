246fb31769d7fdae8ae587a34ea1413fabd12005
pixelea-fr <equipe@pixelea.fr>
2026-04-02 15:06:15 +0200

    ajustement query loop bien

--- patterns/query-loop-bien.php
+++ patterns/query-loop-bien.php
@@ -19,7 +19,19 @@
     <!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|5"}},"layout":{"type":"grid","columnCount":3}} -->
 <!-- wp:pattern {"slug":"ng1-base/card-bien"} /-->
 <!-- /wp:post-template -->
-<!-- wp:pattern {"slug":"ng1-base/no-result-bien"} /-->
+<!-- wp:query-no-results {"align":"wide"} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|7","bottom":"var:preset|spacing|7","left":"var:preset|spacing|2","right":"var:preset|spacing|2"}}},"layout":{"type":"constrained","contentSize":"700px","wideSize":"890px"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--7);padding-right:var(--wp--preset--spacing--2);padding-bottom:var(--wp--preset--spacing--7);padding-left:var(--wp--preset--spacing--2)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"xl"} -->
+<h3 class="wp-block-heading has-text-align-center has-xl-font-size"><strong>Aucun Bien </strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Aucun bien n'est disponible pour le moment.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:query-no-results -->
 <!-- wp:query-pagination {"paginationArrow":"chevron","layout":{"type":"flex","justifyContent":"center"}} -->
 <!-- wp:query-pagination-previous /-->
 <!-- wp:query-pagination-numbers /-->
